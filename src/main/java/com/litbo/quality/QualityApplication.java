67b9db441daf31679fdd25392763b6c4273320b0
litbo <973046515@qq.com>
2018-12-19 18:01:49 +0800

    Initial commit

---
+++ src/main/java/com/litbo/quality/QualityApplication.java
@@ -0,0 +1,14 @@
+package com.litbo.quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QualityApplication.class, args);
+    }
+
+}
+
